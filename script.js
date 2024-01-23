d4947ebea3d24c508eb13f21bb206313657109c3
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-22 21:10:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var map = L.map('map').setView([37.7612887808995, -122.4326410508401], 11);
 
-// load a tile layer
+//basemap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});.addTo(map);
+}).addTo(map);
 
 //load GeoJSON from an external file
 $.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson",function(data){
