c1c3b1bc7ed02e02c9100f532a4f853b687d3280
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-22 21:06:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 var map = L.map('map').setView([37.7612887808995, -122.4326410508401], 11);
 
 // load a tile layer
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-}).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});.addTo(map);
 
 //load GeoJSON from an external file
 $.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson",function(data){
