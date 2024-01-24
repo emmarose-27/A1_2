b13b0eae7647c35eef7413559fa7bf090794f270
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-23 18:57:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{
 
 //load GeoJSON from an external file
 $.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson",function(data){
-  var ratIcon = L.icon({
-      iconUrl: 'http://maptimeboston.github.io/leaflet-intro/rat.gif',
+  var crimeIcon = L.icon({
+      iconUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8V6tCzEs7nLWREM6LKSKqx0zCEkHH5nfJ2g&usqp=CAU',
       iconSize: [50,40]
     });
   // add GeoJSON layer to the map once the file is loaded
   L.geoJson(data,{
       pointToLayer: function(feature,latlng){
-	    return L.marker(latlng,{icon: ratIcon});
+	    return L.marker(latlng,{icon: crimeIcon});
       }
     }).addTo(map);
 });
