941931c471d2ec8320fe032fe49eae5964ba76ab
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-23 18:41:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var map = L.map('map').setView([37.7612887808995, -122.4326410508401], 11);
 
-//basemap
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// load a tile layer
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 }).addTo(map);
 
 //load GeoJSON from an external file
